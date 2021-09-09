b78053e709b8719643f840d614282084e4174c73
Joe Walbran <walbr037@morris.umn.edu>
2021-09-09 18:54:55 -0500

    Tidy up process_client_logs.sh a bit
    Fixing some whitespace, and making sure we exit with code 1 instead of 0
    on error.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-#Make sure that we go into the correct directory
+# Make sure that we go into the correct directory
 dirName="$1"
-cd "$dirName" || exit
+cd "$dirName" || exit 1
 
-#Concatenates all of the secure files together and filters out failed password attempts. Then, it will extact the date, time, username, and IP address of each failed password attempt and then output it into the failed_login_data.txt file.
+# Concatenate all of the secure files together and filter out failed password
+# attempts. Then, extact the date, time, username, and IP address of each
+# failed password attempt, and output it into the failed_login_data.txt file.
 cat var/log/secure* | awk '/Failed password for/ {
   dateInfo=substr($0, 0, index($0, ":") - 1);
   if(index($0, "invalid user") > 0) {
     match($0, /invalid user ([^ ]+) from ([^ ]+)/, groups);
-  }else {
+  } else {
     match($0, /Failed password for ([^ ]+) from ([^ ]+)/, groups);
   }
   username=groups[1];
