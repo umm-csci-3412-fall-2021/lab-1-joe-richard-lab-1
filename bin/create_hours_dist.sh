f02b8bfd4a0ff5eaa6de34e78e8678f38268be96
Richard Lussier <lussi036@morris.umn.edu>
2021-09-12 14:28:43 -0500

    Write create_hours_dist.sh file
    The create_hours_dist.sh file should be complete.
    
    Co-authored-by: helloworldl12321 <walbr037@morris.umn.edu>

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# The path to a directory containing a bunch of subfolders--one for each
+# machine--each subfolder containing a `failed_login_data.txt` file.
+logsDir="$1"
+
+tmpFile="$(mktemp /tmp/tmp.XXXXXXXXXXX)"
+
+#Note that \x27 is the ' character
+cat "$logsDir"/*/failed_login_data.txt \
+  | awk '{ print $3 }' \
+  | sort \
+  | uniq --count \
+  | awk '{ printf("data.addRow([\x27%s\x27, %d]);\n", $2, $1) }' \
+  > "$tmpFile"
+
+#Takes the data in the temp file and adds the correct header and footer,
+#saving the result into data/hours_dist.html
+./bin/wrap_contents.sh "$tmpFile" html_components/hours_dist data/hours_dist.html
+rm "$tmpFile"
