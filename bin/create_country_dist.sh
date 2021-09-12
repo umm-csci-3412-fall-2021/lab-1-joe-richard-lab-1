df13e2867f0567245f2cd2dba7c4525d05c192cc
Richard Lussier <lussi036@morris.umn.edu>
2021-09-12 14:44:10 -0500

    Write create_country_dist.sh file
    Implements the create_country_dist.sh file. This file should be
    complete.
    
    Co-authored-by: helloworld12321 <walbr037@morris.umn.edu>

---
+++ bin/create_country_dist.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# The path to a directory containing a bunch of subfolders--one for each
+# machine--each subfolder containing a `failed_login_data.txt` file.
+logsDir="$1"
+
+tmpFile="$(mktemp /tmp/tmp.XXXXXXXXXXX)"
+
+#Note that \x27 is the ' character
+#Note that the join command matches IP addresses with their country codes,
+#`-o 2.2` will make join only output the country codes.
+cat "$logsDir"/*/failed_login_data.txt \
+  | awk '{ print $5 }' \
+  | sort \
+  | join - etc/country_IP_map.txt -o 2.2 \
+  | sort \
+  | uniq --count \
+  | awk '{ printf("data.addRow([\x27%s\x27, %d]);\n", $2, $1) }' \
+  > "$tmpFile"
+
+#Takes the data in the temp file and adds the correct header and footer,
+#saving the result into data/country_dist.html
+./bin/wrap_contents.sh "$tmpFile" html_components/country_dist data/country_dist.html
+rm "$tmpFile"
